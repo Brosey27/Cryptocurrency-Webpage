0a2d3a40af8dc996d37e04fc476c9ae9cbacb46e
Brosey27 <140609566+Brosey27@users.noreply.github.com>
2023-08-12 15:37:07 +1000

    Add correct api, troubleshooted issue with foreach section, page now outlines correct pricing

---
+++ script.js
@@ -0,0 +1,44 @@
+const cryptoList = document.getElementById("cryptoList");
+
+async function fetchCryptocurrencyData() {
+    const response = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin,ethereum,cardano");
+    const data = await response.json();
+    return data;
+}
+
+async function displayCryptocurrencies() {
+    try {
+        const cryptoData = await fetchCryptocurrencyData();
+
+        if (!Array.isArray(cryptoData)) {
+            console.error("API response is not an array:", cryptoData);
+            return;
+        }
+
+        cryptoData.forEach(crypto => {
+            const cryptoItem = createCryptoItem(crypto);
+            cryptoList.appendChild(cryptoItem);
+        });
+    } catch (error) {
+        console.error("Error fetching cryptocurrency data:", error);
+    }
+    function createCryptoItem(crypto) {
+        const cryptoItem = document.createElement("div");
+        cryptoItem.classList.add("crypto-item");
+        cryptoItem.innerHTML = `
+            <div class="crypto-name">${crypto.name}</div>
+            <div class="crypto-price">$${crypto.current_price.toFixed(2)}</div>
+            <div class="crypto-change ${crypto.price_change_percentage_24h >= 0 ? '' : 'negative'}">${crypto.price_change_percentage_24h.toFixed(2)}%</div>
+        `;
+        return cryptoItem;
+    }
+}
+
+displayCryptocurrencies();
+
+
+
+
+    
+
+
